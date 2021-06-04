248171d7665e2ef29ea983973eabe3e24247f0f4
Shivam Gupta <shiva6588@gmail.com>
2021-06-04 12:13:20 +0530

    working on buttons

--- TokenGenerate.js
+++ TokenGenerate.js
@@ -1,3 +1,13 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+
+
+
+function myFunction1() {
+var data="<form action='details'>Enter the WesiteID gathered by your tracking system to see relevant statistics<hr><br><input type='url'placeholder='WebsiteID:' name='url'/><br>  <input type='submit' value='Check WebsiteID'/></form>";   
+
+    document.getElementById("demo").innerHTML = data;
+  }
+function myFunction2() {
+var data="<form action='details'>Enter the IspID <hr><br><input type='url'placeholder='IspID:' name='url'/><br>  <input type='submit' value='Check IspID'/></form>";   
+    
+    document.getElementById("demo").innerHTML = data;
   }
